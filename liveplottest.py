0f757ade6ed4f0d00fe0126d8ccdd40b66fc4b38
Rebecca Jordan <rebecca.jordan@students.olin.edu>
2016-12-06 18:17:38 -0500

    comment debugging

--- liveplottest.py
+++ liveplottest.py
@@ -10,6 +10,9 @@ from time import sleep
 F = FlexSensor()
 
 for i in range(0, 100):
+    print('reading')
     F.readsensors()
+    print('plotting')
     F.plot
+    print('sleeping')
     sleep(0.25)
